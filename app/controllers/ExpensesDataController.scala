c0646a77bb793f99619054af72b7eb8333621d04
aronchik <aaron.le94@gmail.com>
2021-09-21 16:55:41 +0300

    Updating grid and JSON functionality.

--- app/controllers/ExpensesDataController.scala
+++ app/controllers/ExpensesDataController.scala
@@ -8,16 +8,23 @@ import javax.inject._
 @Singleton
 class ExpensesDataController @Inject() (cc: ControllerComponents) extends AbstractController(cc){
 
-  def expenses = Action { Ok(costData) }
+  /**
+   * Create an Action to send the client side a JSON that contains all the relevant data that is stored in the file.
+   */
+  def expenses = Action { Ok(expenseData) }
 
-  def costData: JsValue = {
+  /**
+   * This function opens the stored file on the system that contains the expenses Information
+   * and returns it in a JSON format.
+   */
+  def expenseData: JsValue = {
     Json.parse("""
     [
-        { "id": 10, "firstName": "Angela", "lastName": "Merkel" },
-        { "id": 20, "firstName": "Vladimir", "lastName": "Putin" },
-        { "id": 30, "firstName": "David", "lastName": "Cameron" },
-        { "id": 40, "firstName": "Barack", "lastName": "Obama" },
-        { "id": 50, "firstName": "François", "lastName": "Hollande" }
+        { "id": 10, "expenseDate": "12.02.2010", "expense": "Coffee" , "amount": "4.22", "paymentType": "Cash", "category": "Food" },
+        { "id": 20, "expenseDate": "12.02.2015", "expense": "Bagel" , "amount": "7.99", "paymentType": "Cash", "category": "Food" },
+        { "id": 30, "expenseDate": "12.02.2011", "expense": "New Car" , "amount": "40800", "paymentType": "Bank Transfer", "category": "Transport" },
+        { "id": 40, "expenseDate": "12.03.2011", "expense": "New Tires" , "amount": "2500", "paymentType": "Credit Card", "category": "Transport" },
+        { "id": 50, "expenseDate": "12.02.2012", "expense": "Overseas Trip" , "amount": "8000", "paymentType": "Credit Card", "category": "Travel" }
     ]
   """)
   }
