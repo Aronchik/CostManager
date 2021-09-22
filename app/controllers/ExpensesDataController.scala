69581069c19a6d50b49a98b61495b05073f8bf1d
aronchik <aaron.le94@gmail.com>
2021-09-22 19:00:12 +0300

    Can now add expenses from client side.

--- app/controllers/ExpensesDataController.scala
+++ app/controllers/ExpensesDataController.scala
@@ -15,7 +15,6 @@ class ExpensesDataController @Inject() (cc: ControllerComponents) extends Abstra
    * Create an Action to send the client side a JSON that contains all the relevant data that is stored in the file.
    */
   def expenses = Action { Ok(expenseData) }
-  addExpense(Json.parse("""{ "id": 60, "expenseDate": "22.22.2222", "expense": "Trip to Space" , "amount": "80000", "paymentType": "Cash", "category": "Travel" , "currency": "UEC" }"""))
 
   /**
    * This function opens the stored file on the system that contains the expenses Information
@@ -24,22 +23,12 @@ class ExpensesDataController @Inject() (cc: ControllerComponents) extends Abstra
   def expenseData: JsValue = {
     val data = Source.fromFile(file).getLines.mkString
     Json.parse(data)
-    /**Json.parse("""
-    [
-        { "id": 10, "expenseDate": "12.02.2010", "expense": "Coffee" , "amount": "4.22", "paymentType": "Cash", "category": "Food", "currency": "NIS" },
-        { "id": 20, "expenseDate": "12.02.2015", "expense": "Bagel" , "amount": "7.99", "paymentType": "Cash", "category": "Food" , "currency": "NIS" },
-        { "id": 30, "expenseDate": "12.02.2011", "expense": "New Car" , "amount": "40800", "paymentType": "Bank Transfer", "category": "Transport" , "currency": "NIS" },
-        { "id": 40, "expenseDate": "12.03.2011", "expense": "New Tires" , "amount": "2500", "paymentType": "Credit Card", "category": "Transport" , "currency": "NIS" },
-        { "id": 50, "expenseDate": "12.02.2012", "expense": "Overseas Trip" , "amount": "8000", "paymentType": "Credit Card", "category": "Travel" , "currency": "NIS" }
-    ]
-  """)**/
   }
 
   /**
    * save the current Expenses to a Json file
    * @param dataToSave - The current overall expenses we want to save
    */
-
   def saveToJson(dataToSave: JsValue) = {
     val writer = new BufferedWriter(new FileWriter(file))
     writer.write(dataToSave.toString())
@@ -55,4 +44,14 @@ class ExpensesDataController @Inject() (cc: ControllerComponents) extends Abstra
     saveToJson(newExpenses)
   }
 
+  /**
+   * Recieve a string from a client using a POST and add it to the server stored JSON.
+   * @param expenseToAdd - The expense we want to add
+   */
+  def addExpenseFromClient (expenseToAdd: String) = Action {
+    val replacements = Map("%22".r -> "\"", "%7B".r -> "{", "%7D".r -> "}", "%20".r -> " ")
+    val replaced = replacements.foldLeft(expenseToAdd) { (s, r) => r._1.replaceAllIn(s, r._2) }
+    addExpense(Json.parse(replaced))
+    Ok(expenseData)
+  }
 }
